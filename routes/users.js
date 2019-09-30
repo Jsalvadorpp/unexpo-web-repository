d01a6b0f36816b3d176004d481d31a0f15f69c11
Jsalvadorpp <josesalvadorpp@hotmail.com>
2019-09-29 20:12:08 -0400

    user can register

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,101 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 
-//+ GET user/register 
-router.get('/register', function(req, res, next) {
+
+//= getting data from users database
+var users = require('../models/users');
+
+//==============================
+//= user/register route
+router.get('/register', function(req, res) {
   const pageData = {
-    page: 'sign up',
+    page: 'Sign up',
     menuId: 'sign up'
   };
 
   res.render('register', pageData);
 });
+//= get user data from sign up form
+router.post('/register', [
+    //= check input
 
-//+ GET user/login
-router.get('/login', function(req, res, next) {
+    check('username')
+      .not().isEmpty().withMessage('username is required')
+      .custom( async (value) => {
+        const foundUser = await users.findOne ({ username : value });
+        if(foundUser){
+          throw new Error('username is already taken');
+        }else{
+          return true;
+        }
+      }),
+
+    check('email')
+      .not().isEmpty().withMessage('email is required')
+      .isEmail().withMessage('invalid email')
+      .normalizeEmail()
+      .custom( async (value) => {
+        const foundUser = await users.findOne ({ email : value });
+        if(foundUser){
+          throw new Error('email is already in use');
+        }else{
+          return true;
+        }
+      }),
+
+    check('password1')
+      .not().isEmpty().withMessage('password is required')
+      .custom( (value,{ req }) => {
+        if(value != req.body.password2){
+          throw new Error('passwords dont match');
+        }else {
+          return true;
+        }
+    }),
+
+    check('password2')
+      .not().isEmpty().withMessage('confirm password is required')
+
+  ],(req,res) =>{ 
+  //= handle request and response
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors);
+  }else{
+
+    //= if no erros then insert new user into the database
+    user = new users({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password1
+    });
+ 
+    user.save( (err,savedUser) => {
+      if (err)  return console.error(err);
+    });
+
+    res.render('login',
+    {
+      page: 'Login',
+      menuId: 'login'
+    });
+
+  }
+
+});
+
+
+
+
+//==============================
+
+//= user/login route
+router.get('/login', function(req, res) {
   const pageData = {
-    page: 'login',
+    page: 'Login',
     menuId: 'login'
   };
 
