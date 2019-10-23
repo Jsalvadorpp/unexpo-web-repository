aebc87c2a2d6985791fc896e962727254573b41a
Jsalvadorpp <josesalvadorpp@hotmail.com>
2019-10-23 17:14:22 -0400

    add validation for duplicate files

---
+++ models/uploads.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+// upload Schema
+const UploadSchema = new Schema({
+    createdBy: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    filename: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        required: true
+    },
+    mimetype:{
+        type: String,
+        required: true
+    },
+    fileId: {
+        type: ObjectId,
+        required: true
+    }
+});
+
+const Upload = module.exports = mongoose.model('Upload',UploadSchema);
--- routes/upload.js
+++ routes/upload.js
@@ -4,6 +4,11 @@ const ensureAuth = require('../config/ensureAuth');
 const GridFsStorage = require('multer-gridfs-storage');
 const multer = require('multer');
 const database = require('../config/databaseConfig');
+const mongoose = require('mongoose');
+const Grid = require('gridfs-stream');
+
+//= getting data from upload database
+var uploads = require('../models/uploads');
 
 //= get upload page
 router.get('/', ensureAuth, (req, res, next) => {
@@ -13,11 +18,15 @@ router.get('/', ensureAuth, (req, res, next) => {
   };
 
   res.render('upload', pageData);
-
 });
 
-//= database instance
+//= database instance and gfs config
+var gfs;
 const connection = database.connect;
+const conn = mongoose.connection;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+});
 
 //= create storage engine to save files
 //= information added to the file when uploading
@@ -26,24 +35,17 @@ const storage = new GridFsStorage({
   file: (req, file) => {
     return {
       filename: file.originalname,
-      metadata: {
-        createdBy: req.user.username,
-        something : req.body.category
-      },
       bucketName: 'uploads'
     }
   }
 });
 
-//======================================================
-//== reminder to myself : clean this code PLEASEEEEEE
-//======================================================
-
 //= function to validate data from upload form
 const uploadValidation = (req,file,cb) => {
   
   var errors = [];
-  
+
+  //= check form inputs
   if(typeof req.body.title === 'undefined' || req.body.title == '')  errors.push('title required');
   if(typeof req.body.description === 'undefined' || req.body.description == '')  errors.push('description required');
 
@@ -53,14 +55,24 @@ const uploadValidation = (req,file,cb) => {
     errors.forEach( error => {
       req.flash('danger',error);
     });
-    cb('fields required');
+    cb('error uploading');
 
   }else{
-    cb(null, true);
-  }
 
+    //= check if the title is already saved in Db
+    uploads.findOne( {title: req.body.title}, (err,file) => {
+      if(file){
+        req.flash('danger','use another title');
+        cb('error uploading');
+      }else{
+        //= upload file 
+        cb(null, true);
+      }
+    });
+  }
 };
 
+//= upload config using multer
 const megabyte = 1024*1024;
 const upload = multer({ 
   storage , 
@@ -74,30 +86,59 @@ router.post('/', (req,res) => {
   //= trying to upload the file
   upload(req,res,(err) => {
 
+    //= file not uploaded 
     if(err) {
       
       if(err.message == 'File too large') req.flash('danger',err.message);
       res.redirect('upload');
     
+    //= file uploaded
     }else{
 
+      //= if no file was submitted then return to upload page
       if(typeof req.file === 'undefined'){
 
         req.flash('danger','file required');
         res.redirect('upload');
-
+      
+      //= trying to create file data 
       }else{
+        //= check if there's a file with same size and mimetype in DB
+        uploads.findOne({size: req.file.size,mimetype: req.file.mimetype},(err,file) => {
+          //= if there's a file then delete the file that was uploaded and return the upload page
+          if(file){
 
-        //= handle request and response
-        const pageData = {
-          page: 'Upload'
-        };
-        res.json({file: req.file});
+            req.flash('danger','file is already into database , please upload another file');
+            //= remove duplicate file
+            gfs.remove({_id: req.file.id , root: 'uploads'});
+            res.redirect('upload');
 
+          //= if there's no duplicate file then insert file data into Db
+          }else{
+            //= insert file data into database (file not included - the file saves in uploads.files)
+            const file = new uploads({
+            createdBy: req.user.username,
+            title: req.body.title,
+            filename: req.file.filename,
+            description: req.body.description,
+            category: req.body.category,
+            size: req.file.size,
+            mimetype: req.file.mimetype,
+            fileId: req.file.id
+            });
+
+            //= save file data
+            file.save( (err,savedFile) => {
+              if (err)  return console.error(err);
+            });
+            
+            req.flash('success','file uploaded!');
+            res.render('homepage',{page: 'home'});
+          }
+        });
       }
     }
   });
-
 });
 
 module.exports = router;
