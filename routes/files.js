449e89322a690168b09905c33fbaceb86ce0082b
Jsalvadorpp <josesalvadorpp@hotmail.com>
2019-10-29 09:25:14 -0400

    add download functionality

--- routes/files.js
+++ routes/files.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Grid = require('gridfs-stream');
 
 //= getting data from upload database
 var files = require('../models/uploads');
+
+//= database instance and gfs config
+var gfs;
+const conn = mongoose.connection;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+});
 
 //= view all files by category
 router.get('/', (req, res, next) => {
@@ -19,6 +28,7 @@ router.get('/', (req, res, next) => {
     .exec( (err,docs)=>{
   
       files.countDocuments({category : category}).exec( (err,count) => {
+
         if(err){
           return console.log(err); 
 
@@ -58,18 +68,56 @@ router.get('/', (req, res, next) => {
 //= view indiviual file
 router.get('/viewFile', (req,res,next) => {
 
+  //= file metadata id
   fileId = req.query.id;
 
   files.findById(fileId).exec( (err,file) => {
 
     if(!file) res.status(404).json({message : 'data not found'});
 
-
-    
     res.render('view',{file,page: `${file.title}`});
   });
 
+});
 
+//= download file
+router.get('/download', (req,res,next) => {
+
+  //= file metadata id
+  id = req.query.id;
+
+  //= look for file data by id
+  files.findById(id).exec( (err,data) => {
+
+    if(!data) res.status(404).json({message : 'data not found'});
+
+    gfs.collection('uploads');
+
+    //= look for the file itself
+    gfs.files.findOne({_id: data.fileId} , (err,file) => {
+      
+      //= get file extension
+      const extension = '.'+file.filename.split('.').pop();
+  
+      //= set file type
+      res.set('Content-Type', file.contentType);
+      //= set filename
+      res.attachment(data.title.trim()+extension);
+
+      //= read file
+      //! DeprecationWarning: GridStore is deprecated
+      const readstream = gfs.createReadStream({
+        _id: data.fileId,
+      });
+
+      readstream.on('error', function (err) {
+        console.log('An error occurred! ', err);
+      });
+
+      //= download file
+      readstream.pipe(res);
+    }); 
+  });
 });
 
 module.exports = router;
