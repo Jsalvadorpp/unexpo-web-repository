b00f68735004f342617a6ce53c2dd70a9d34b476
Jsalvadorpp <josesalvadorpp@hotmail.com>
2019-11-04 13:31:02 -0400

    test2

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -4,7 +4,11 @@ require('dotenv').config()
 const databaseName = 'Unexpo_repository';
 
 const dbConnection = mongoose.connect(process.env.MONGODB_URI, 
-{useNewUrlParser: true,useUnifiedTopology: true ,useCreateIndex: true});
+{useNewUrlParser: true,useUnifiedTopology: true ,useCreateIndex: true})
+
+dbConnection.then( () => {
+    console.log('connected to mongo');
+});
 
 module.exports.databaseName = databaseName;
 module.exports.connect = dbConnection;
