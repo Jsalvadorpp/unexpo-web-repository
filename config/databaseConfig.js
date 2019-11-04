ee881f0cca357cf4d60809a048eb08251d7cd930
Jsalvadorpp <josesalvadorpp@hotmail.com>
2019-11-04 13:37:16 -0400

    help!

--- config/databaseConfig.js
+++ config/databaseConfig.js
@@ -6,9 +6,5 @@ const databaseName = 'Unexpo_repository';
 const dbConnection = mongoose.connect(process.env.MONGODB_URI, 
 {useNewUrlParser: true,useUnifiedTopology: true ,useCreateIndex: true})
 
-dbConnection.then( () => {
-    console.log('connected to mongo');
-});
-
 module.exports.databaseName = databaseName;
 module.exports.connect = dbConnection;
